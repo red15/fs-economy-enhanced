28ca03f9d272a60a7680e50d3933ce49fe2c9def
Niels Huylebroeck <github@nightwalkers.be>
2014-12-19 01:31:16 +0100

    Improved url includes and allow multiple tables.

--- fse-enhanced.js
+++ fse-enhanced.js
@@ -2,42 +2,63 @@
 // @name        FS Economy Enhancement
 // @namespace   nightwalkers
 // @description Improvements for FS Economy usability
-// @include     http://server.fseconomy.net/airport.jsp*
-// @include     http://server.fseconomy.net:81/airport.jsp*
-// @version     1
+// @include     http://server.fseconomy.net/*
+// @include     http://server.fseconomy.net:81/*
+// @version     2
 // @grant       none
 // ==/UserScript==
 
-// Poor man's jQuery
-table = document.getElementsByTagName('table');
-for (var i = 0; i < table.length; i++) {
-  if (table[i].classList.contains('assigmentTable')) {
-    table = table[i];
-    break;
-  }
-}
+var tables = document.getElementsByTagName('table');
+for (var i = 0; i < tables.length; i++) {
+    var table = tables[i];
+    if (!tables[i].classList.contains('assigmentTable')) continue;
+    // Poor man's jQuery
+    console.debug('Table found', table);
 
-// Adding column header
-col_head = document.createElement('th');
-col_head.textContent = 'Price per nm/qty';
-table.rows[0].appendChild(col_head);
+    // Adding column header
+    var col_head = document.createElement('th');
+    col_head.textContent = 'Price per nm/qty';
+    table.rows[0].appendChild(col_head);
 
-// Skipping first row (header)
-for (var x = 1; x < table.rows.length; x++) {
-  row = table.rows[x];
+    var col_pay, col_nm, col_cargo;
+    // Skipping first row (header)
+    for (var x = 0; x < table.rows.length; x++) {
+        var row = table.rows[x];
+        if (x == 0) {
+            for (var y = 0; y < row.cells.length; y++) {
+                var cell = row.cells[y];
+                // Find the table headings
+                if (cell.textContent.contains('Pay')) {
+                    console.debug('Payment is in column', y);
+                    col_pay = y;
+                }
+                if (cell.textContent.contains('NM')) {
+                    console.debug('Distance is in column', y);
+                    col_nm = y;
+                }
+                if (cell.textContent.contains('Cargo')) {
+                    console.debug('Cargo is in column', y);
+                    col_cargo = y;
+                }
+            }
+        } else {
+            var qty = parseFloat(row.cells[col_cargo].textContent.split(' ')[0]) || 1,
+                range = parseFloat(row.cells[col_nm].textContent),
+                price = parseFloat(row.cells[col_pay].textContent.replace('$', '').replace(',','')),
+                calc = 0, rounding = 2;
 
-  qty = parseFloat(row.cells[6].textContent.split(' ')[0]) || 1;
-  range = parseFloat(row.cells[4].textContent);
-  price = parseFloat(row.cells[1].textContent.replace('$', '').replace(',',''));
+            if (qty && range && price) {
+                while (calc === 0) {
+                    calc = Math.round( (price / qty / range) * Math.pow(10, rounding) ) / Math.pow(10, rounding);
+                    rounding++;
+                }                
+            } else {
+                calc == NaN;
+            }
 
-  rounding = 2;
-  calc = 0;
-  while (calc === 0) {
-    calc = Math.round( (price / qty / range) * Math.pow(10, rounding) ) / Math.pow(10, rounding);
-    rounding++;
-  }
-
-  new_cell = document.createElement('td');
-  new_cell.textContent = '$'+calc.toString();
-  row.appendChild(new_cell);
+            new_cell = document.createElement('td');
+            new_cell.textContent = '$'+calc.toString();
+            row.appendChild(new_cell);
+        }
+    }
 }
